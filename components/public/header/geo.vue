f64f2404f070140f79ba89b2523466a1b1a17648
weiss <1297663809@qq.com>
2019-12-23 19:29:31 +0800

    header

---
+++ components/public/header/geo.vue
@@ -0,0 +1,14 @@
+<template>
+   	<div class="m-geo">
+		<i class="el-icon-location">
+			北京
+			<!-- {{ $store.state.geo.position.city }} -->
+		</i>
+		<nuxt-link 
+			class="changeCity"
+			to="/changeCity">
+			切换城市
+		</nuxt-link>
+		[廊坊 河北 固安]
+	</div>
+</template>
